d5863aaea1bb9fee358a6f632373e00bef870ba4
DESKTOP-AF5A5H4\dongkyu <kdk8747@naver.com>
2024-07-07 21:56:49 +0900

    add a crate vulkano

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::{Version, VulkanLibrary};
+
+
 fn main() {
-    println!("Hello, World!");
+    let instance = {
+        let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+        //let extensions = vulkano_win::required_extensions(&library);
+
+        Instance::new(
+            library,
+            InstanceCreateInfo {
+                //enabled_extensions: extensions,
+                max_api_version: Some(Version::V1_2),
+                ..Default::default()
+            },
+        )
+        .expect("failed to create instance")
+    };
+
+    let physical_device = instance
+        .enumerate_physical_devices()
+        .expect("could not enumerate devices")
+        .next()
+        .expect("no devices available");
+
+    println!(
+        "Using device: {} (type: {:?})",
+        physical_device.properties().device_name,
+        physical_device.properties().device_type
+    );
+    println!(
+        "Our physical device supports Vulkan: {:?}",
+        physical_device.properties().api_version
+    );
 }
